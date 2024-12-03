bda8fa0b8756ec575f328662719a5f4824c1c050
Daniel Conger <dconger3@gmail.com>
2024-12-03 14:22:44 -0700

    update

---
+++ src/components/demo.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@/components/ui/button';
+
+export const Demo = () => (
+  <div className="not-prose flex aspect-video items-center justify-center rounded-lg border bg-secondary">
+        <Button variant="outline">Show me</Button>
+  </div>
+);
